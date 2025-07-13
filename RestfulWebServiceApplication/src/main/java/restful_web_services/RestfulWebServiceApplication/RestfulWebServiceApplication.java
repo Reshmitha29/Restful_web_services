5c0227364571f64d07d5f9e5ff38f0348ec97625
Reshmitha29 <29reshmithavaishnaviv@gmail.com>
2025-07-13 16:26:59 +0530

    initializing a maven project for rest web app

---
+++ RestfulWebServiceApplication/src/main/java/restful_web_services/RestfulWebServiceApplication/RestfulWebServiceApplication.java
@@ -0,0 +1,13 @@
+package restful_web_services.RestfulWebServiceApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServiceApplication.class, args);
+	}
+
+}
---
+++ RestfulWebServiceApplication/src/test/java/restful_web_services/RestfulWebServiceApplication/RestfulWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package restful_web_services.RestfulWebServiceApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
